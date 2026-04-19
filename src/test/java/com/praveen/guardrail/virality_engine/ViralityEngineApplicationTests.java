bb4f4595d8053059750565192234ec06c42ec797
Praveen Kumar <praveen729483@gmail.com>
2026-04-20 01:41:42 +0530

    Setup initial project development

---
+++ src/test/java/com/praveen/guardrail/virality_engine/ViralityEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.guardrail.virality_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViralityEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
