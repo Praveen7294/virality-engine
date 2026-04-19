31e10594609b23a13e769aa5b678c89123b4492f
Praveen Kumar <praveen729483@gmail.com>
2026-04-20 02:17:25 +0530

    Added entity Bot.java

---
+++ src/main/java/com/praveen/guardrail/virality_engine/entity/Bot.java
@@ -0,0 +1,55 @@
+package com.praveen.guardrail.virality_engine.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bots")
+public class Bot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "persona_description")
+    private String personaDescription;
+
+    public Bot() {
+    }
+
+    public Bot(String name, String personaDescription) {
+        this.name = name;
+        this.personaDescription = personaDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonaDescription() {
+        return personaDescription;
+    }
+
+    public void setPersonaDescription(String personaDescription) {
+        this.personaDescription = personaDescription;
+    }
+}
