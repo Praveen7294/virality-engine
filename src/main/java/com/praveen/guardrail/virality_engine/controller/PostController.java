178a2e5fdc307d33eb3adcf02b77a9ef8ecc4a68
Praveen Kumar <praveen729483@gmail.com>
2026-04-20 16:53:33 +0530

    Added a PostController.java

---
+++ src/main/java/com/praveen/guardrail/virality_engine/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.praveen.guardrail.virality_engine.controller;
+
+import com.praveen.guardrail.virality_engine.dto.PostRequestDTO;
+import com.praveen.guardrail.virality_engine.dto.PostResponseDTO;
+import com.praveen.guardrail.virality_engine.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostResponseDTO> createPost(@RequestBody PostRequestDTO postRequestDTO) {
+
+        PostResponseDTO postResponseDTO = postService.createPost(postRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(postResponseDTO);
+    }
+}
